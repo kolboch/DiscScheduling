70d3b0647aab3abfc773a3bd203e4427d088741f
Karol Bochynski <kolboch@gmail.com>
2016-05-03 12:54:24 +0200

    next request method and hasNextrequest added some must have methods like addDistanceCovered and ones controlling timeElapsed and currentTime

--- src/disk/DiscResource.java
+++ src/disk/DiscResource.java
@@ -1,32 +1,31 @@
 package disk;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+import diskTasks.ApproachRequestComparator;
 import diskTasks.Request;
 
 public class DiscResource {
 	
-	private static int numberOfCylinders;
+	private int numberOfCylinders;
 	private int distanceCovered;
-	Queue<Request> requestQueue;
-	private Comparator<Request> approachComparator;
-	private Comparator<Request> diskDistanceComparator;
+	private int currentTime;
+	
+	private Queue<Request> requestQueue;
+	private Queue<Request> realTimeRequestQueue;
 	
 	public DiscResource(int numberOfCylinders){
 		this.numberOfCylinders = numberOfCylinders;
-		this.requestQueue = new PriorityQueue<>();
+		this.requestQueue = new PriorityQueue<>(new ApproachRequestComparator());
+		this.realTimeRequestQueue = new PriorityQueue<>(new ApproachRequestComparator());
 	}
-	
-	
-	
 	
 	public int getDistanceCovered(){
 		return this.distanceCovered;
 	}
 	
-	private static int getNumberOfCylinders(){
+	private int getNumberOfCylinders(){
 		return numberOfCylinders;
 	}
 	
@@ -35,8 +34,57 @@ public class DiscResource {
 	}
 	
 	private Request getNextRequest(){
-		//TODO
-		return null;
 		
+		if(!realTimeRequestQueue.isEmpty()){
+			if(realTimeRequestQueue.peek().getApproachTime() > getCurrentTime()){
+				return realTimeRequestQueue.poll();
+			}
+		}
+		if(!requestQueue.isEmpty()){
+			
+			Request nextRequest = null;
+			
+			for(Request r : requestQueue){
+				
+				// for not FCFS algorithm we will change comparator 
+				// so we will get valid request but sorted according to actual comparator
+				if(r.getApproachTime() > getCurrentTime()){ 
+					nextRequest = r;
+					break;
+				}
+				
+			}
+			requestQueue.remove(nextRequest);
+			return nextRequest;
+		}
+		else{
+			return null;
+		}
+	}
+	
+	private boolean hasNextRequest(){
+		
+		if(!realTimeRequestQueue.isEmpty()){
+			return true;
+		}
+		else if(!requestQueue.isEmpty()){
+			return true;
+		}
+		else{
+			return false;
+		}
+		
+	}
+	
+	private int getCurrentTime(){
+		return this.currentTime;
+	}
+	
+	private void addElapsedTime(int timeElapsed){
+		this.currentTime += timeElapsed;
+	}
+	
+	private void addDistanceCovered(int distanceCovered){
+		this.distanceCovered += distanceCovered;
 	}
 }
